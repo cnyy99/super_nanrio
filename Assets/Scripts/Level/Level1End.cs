00009b04f1f831f2397ec7681d9aa57f78336054
cnyy <chennansoul@gmail.com>
2019-05-19 18:24:57 +0800

    add level2Preload

---
+++ Assets/Scripts/Level/Level1End.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1End : MonoBehaviour
+{
+    public GameObject FadeScreen;
+    public GameObject ThePlayer;
+
+    IEnumerator OnTriggerEnter()
+    {
+        FadeScreen.SetActive(true);
+        FadeScreen.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(0.495F);
+        FadeScreen.GetComponent<Animator>().enabled = false;
+        Application.LoadLevel(4);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
